06abf414028092126fdcf359d2b74e596bde0893
jess <jess1104jess1104@gmail.com>
2021-11-03 16:27:12 +0800

    搬移store,scss修改,uprecord,downrecord,add read.md

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="app">
     <router-view />
   </div>
 </template>
@@ -17,7 +17,7 @@ body {
   align-items: center;
 }
 
-#app {
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/store/state.js
@@ -0,0 +1,8 @@
+const state = {
+  todos: [
+    { id: 1, content: 123, done: false },
+    { id: 2, content: 456, done: true },
+    { id: 3, content: 789, done: false },
+  ],
+};
+export default state;
